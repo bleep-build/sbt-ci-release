7d61448b8e6f7961df7cdfc1e927702c6beacb06
Øyvind Raddum Berg <oyvind.raddum.berg@nrk.no>
2022-04-29 08:54:05 +0200

    port to bleep

--- plugin/src/main/scala/com/geirsson/CiReleasePlugin.scala
+++ plugin/src/main/scala/com/geirsson/CiReleasePlugin.scala
@@ -1,35 +1,60 @@
 package com.geirsson
 
-import com.typesafe.sbt.GitPlugin
-import com.typesafe.sbt.SbtGit.GitKeys
-import com.jsuereth.sbtpgp.SbtPgp
-import com.jsuereth.sbtpgp.SbtPgp.autoImport._
-import java.nio.file.Files
-import java.nio.file.Paths
-import java.nio.charset.StandardCharsets
+import bleep.RelPath
+import bleep.internal.FileUtils
+import bleep.logging.Logger
+import com.geirsson.CiReleasePlugin._
+import nosbt.librarymanagement.ScmInfo
+import bleep.tasks.publishing._
+
+import java.net.URL
+import java.nio.file.{Files, Path}
 import java.util.Base64
-import sbt.Def
-import sbt.Keys._
-import sbt._
-import sbt.plugins.JvmPlugin
-import sbtdynver.DynVerPlugin
-import sbtdynver.DynVerPlugin.autoImport._
-import scala.deprecated
 import scala.sys.process._
 import scala.util.control.NonFatal
-import xerial.sbt.Sonatype
-import xerial.sbt.Sonatype.autoImport._
 
-object CiReleasePlugin extends AutoPlugin {
+class CiReleasePlugin(val logger: Logger, val sonatype: Sonatype, val dynVer: DynVerPlugin, val pgp: PgpPlugin) {
 
-  override def trigger = allRequirements
-  override def requires =
-    JvmPlugin && SbtPgp && DynVerPlugin && GitPlugin && Sonatype
+  def ciRelease(files: Map[RelPath, Array[Byte]]): Unit =
+    if (!isSecure) {
+      sys.error("No access to secret variables, doing nothing")
+    } else {
+      logger.withContext(currentBranch).info(s"Running ci-release")
+      setupGpg()
+      if (!isTag) {
+        if (isSnapshotVersion(dynVer.version)) {
+          logger.info(s"No tag push, publishing SNAPSHOT")
+          require(dynVer.dynverSonatypeSnapshots, "dynVer.config.dynverSonatypeSnapshots must be true")
+          sys.error("publish snapshots not implemented yet")
+//          publishSnapshots()
+        } else {
+          // Happens when a tag is pushed right after merge causing the master branch
+          // job to pick up a non-SNAPSHOT version even if TRAVIS_TAG=false.
+          sys.error("Snapshot releases must have -SNAPSHOT version number, doing nothing")
+        }
+      } else {
+        logger.info("Tag push detected, publishing a stable release")
+        sonatype.sonatypeClean()
 
+        val synced = FileUtils.syncBytes(
+          sonatype.sonatypeBundleDirectory,
+          files,
+          FileUtils.DeleteUnknowns.Yes(maxDepth = None),
+          soft = false
+        )
+        pgp.signedArtifacts(synced.keys)
+
+        sonatype.sonatypeBundleRelease()
+      }
+    }
+}
+
+object CiReleasePlugin {
   def isSecure: Boolean =
     System.getenv("TRAVIS_SECURE_ENV_VARS") == "true" ||
       System.getenv("BUILD_REASON") == "IndividualCI" ||
       System.getenv("PGP_SECRET") != null
+
   def isTag: Boolean =
     Option(System.getenv("TRAVIS_TAG")).exists(_.nonEmpty) ||
       Option(System.getenv("CIRCLE_TAG")).exists(_.nonEmpty) ||
@@ -37,6 +62,7 @@ object CiReleasePlugin extends AutoPlugin {
       Option(System.getenv("BUILD_SOURCEBRANCH"))
         .orElse(Option(System.getenv("GITHUB_REF")))
         .exists(_.startsWith("refs/tags"))
+
   def releaseTag: String =
     Option(System.getenv("TRAVIS_TAG"))
       .orElse(Option(System.getenv("BUILD_SOURCEBRANCH")))
@@ -44,6 +70,7 @@ object CiReleasePlugin extends AutoPlugin {
       .orElse(Option(System.getenv("CIRCLE_TAG")))
       .orElse(Option(System.getenv("CI_COMMIT_TAG")))
       .getOrElse("<unknown>")
+
   def currentBranch: String =
     Option(System.getenv("TRAVIS_BRANCH"))
       .orElse(Option(System.getenv("BUILD_SOURCEBRANCH")))
@@ -51,15 +78,6 @@ object CiReleasePlugin extends AutoPlugin {
       .orElse(Option(System.getenv("CIRCLE_BRANCH")))
       .orElse(Option(System.getenv("CI_COMMIT_BRANCH")))
       .getOrElse("<unknown>")
-
-  @deprecated("Deprecated, please use isSecure", "1.4.32")
-  def isTravisSecure: Boolean = isSecure
-  @deprecated("Deprecated, please use isTag", "1.4.32")
-  def isTravisTag: Boolean = isTag
-  @deprecated("Deprecated, please use releaseTag", "1.4.32")
-  def travisTag: String = releaseTag
-  @deprecated("Deprecated, please use currentBranch", "1.4.32")
-  def travisBranch: String = currentBranch
 
   def isAzure: Boolean =
     System.getenv("TF_BUILD") == "True"
@@ -72,11 +90,11 @@ object CiReleasePlugin extends AutoPlugin {
 
   def setupGpg(): Unit = {
     val versionLine = List("gpg", "--version").!!.linesIterator.toList.head
-    println(versionLine)
+
     val TaggedVersion = """(\d{1,14})([\.\d{1,14}]*)((?:-\w+)*)""".r
     val gpgVersion: Long = versionLine.split(" ").last match {
       case TaggedVersion(m, _, _) => m.toLong
-      case _                         => 0L
+      case _                      => 0L
     }
     // https://dev.gnupg.org/T2313
     val importCommand =
@@ -86,7 +104,7 @@ object CiReleasePlugin extends AutoPlugin {
     if (isAzure) {
       // base64 encoded gpg secrets are too large for Azure variables but
       // they fit within the 4k limit when compressed.
-      Files.write(Paths.get("gpg.zip"), Base64.getDecoder.decode(secret))
+      Files.write(Path.of("gpg.zip"), Base64.getDecoder.decode(secret))
       s"unzip gpg.zip".!
       s"gpg $importCommand gpg.key".!
     } else {
@@ -96,89 +114,30 @@ object CiReleasePlugin extends AutoPlugin {
 
   private def gitHubScmInfo(user: String, repo: String) =
     ScmInfo(
-      url(s"https://github.com/$user/$repo"),
+      new URL(s"https://github.com/$user/$repo"),
       s"scm:git:https://github.com/$user/$repo.git",
       Some(s"scm:git:git@github.com:$user/$repo.git")
     )
 
-  override lazy val buildSettings: Seq[Def.Setting[_]] = List(
-    dynverSonatypeSnapshots := true,
-    scmInfo ~= {
-      case Some(info) => Some(info)
-      case None =>
-        import scala.sys.process._
-        val identifier = """([^\/]+?)"""
-        val GitHubHttps = s"https://github.com/$identifier/$identifier(?:\\.git)?".r
-        val GitHubGit   = s"git://github.com:$identifier/$identifier(?:\\.git)?".r
-        val GitHubSsh   = s"git@github.com:$identifier/$identifier(?:\\.git)?".r
-        try {
-          val remote = List("git", "ls-remote", "--get-url", "origin").!!.trim()
-          remote match {
-            case GitHubHttps(user, repo) => Some(gitHubScmInfo(user, repo))
-            case GitHubGit(user, repo)   => Some(gitHubScmInfo(user, repo))
-            case GitHubSsh(user, repo)   => Some(gitHubScmInfo(user, repo))
-            case _                       => None
-          }
-        } catch {
-          case NonFatal(_) => None
-        }
-    }
-  )
-
-  override lazy val globalSettings: Seq[Def.Setting[_]] = List(
-    publishArtifact.in(Test) := false,
-    publishMavenStyle := true,
-    commands += Command.command("ci-release") { currentState =>
-      if (!isSecure) {
-        println("No access to secret variables, doing nothing")
-        currentState
-      } else {
-        println(
-          s"Running ci-release.\n" +
-            s"  branch=$currentBranch"
-        )
-        setupGpg()
-        // https://github.com/olafurpg/sbt-ci-release/issues/64
-        val reloadKeyFiles =
-          "; set pgpSecretRing := pgpSecretRing.value; set pgpPublicRing := pgpPublicRing.value"
-        if (!isTag) {
-          if (isSnapshotVersion(currentState)) {
-            println(s"No tag push, publishing SNAPSHOT")
-            reloadKeyFiles ::
-              sys.env.getOrElse("CI_SNAPSHOT_RELEASE", "+publish") ::
-              currentState
-          } else {
-            // Happens when a tag is pushed right after merge causing the master branch
-            // job to pick up a non-SNAPSHOT version even if TRAVIS_TAG=false.
-            println(
-              "Snapshot releases must have -SNAPSHOT version number, doing nothing"
-            )
-            currentState
-          }
-        } else {
-          println("Tag push detected, publishing a stable release")
-          reloadKeyFiles ::
-            sys.env.getOrElse("CI_CLEAN", "; clean ; sonatypeBundleClean") ::
-            sys.env.getOrElse("CI_RELEASE", "+publishSigned") ::
-            sys.env.getOrElse("CI_SONATYPE_RELEASE", "sonatypeBundleRelease") ::
-            currentState
-        }
+  def inferScmInfo: Option[ScmInfo] = {
+    import scala.sys.process._
+    val identifier = """([^\/]+?)"""
+    val GitHubHttps = s"https://github.com/$identifier/$identifier(?:\\.git)?".r
+    val GitHubGit = s"git://github.com:$identifier/$identifier(?:\\.git)?".r
+    val GitHubSsh = s"git@github.com:$identifier/$identifier(?:\\.git)?".r
+    try {
+      val remote = List("git", "ls-remote", "--get-url", "origin").!!.trim()
+      remote match {
+        case GitHubHttps(user, repo) => Some(gitHubScmInfo(user, repo))
+        case GitHubGit(user, repo)   => Some(gitHubScmInfo(user, repo))
+        case GitHubSsh(user, repo)   => Some(gitHubScmInfo(user, repo))
+        case _                       => None
       }
-    }
-  )
-
-  override lazy val projectSettings: Seq[Def.Setting[_]] = List(
-    publishConfiguration :=
-      publishConfiguration.value.withOverwrite(true),
-    publishLocalConfiguration :=
-      publishLocalConfiguration.value.withOverwrite(true),
-    publishTo := sonatypePublishToBundle.value
-  )
-
-  def isSnapshotVersion(state: State): Boolean = {
-    version.in(ThisBuild).get(Project.extract(state).structure.data) match {
-      case Some(v) => v.endsWith("-SNAPSHOT")
-      case None    => throw new NoSuchFieldError("version")
+    } catch {
+      case NonFatal(_) => None
     }
   }
+
+  def isSnapshotVersion(v: String): Boolean =
+    v.endsWith("-SNAPSHOT")
 }
